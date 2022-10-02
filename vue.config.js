27f79bf6713e20eef02321e1aca115ac6171b221
ComingEvanYou <2045819657@qq.com>
2022-10-02 21:59:50 +0800

    feat(custom): 配置跨域

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    proxy:{
+      "/app-api":{
+        target:"",
+        changeOrigin:true,
+        pathRewrite:{
+          "^/app-api":""
+        }
+      }
+    }
+  }
 })
