7c590573f771de5ba03929e72d96f0e2cc5cb2c7
ComingEvanYou <2045819657@qq.com>
2022-10-02 21:22:42 +0800

    feat(custom): 封装axios

---
+++ src/utils/requset.js
@@ -0,0 +1,28 @@
+
+//引入axios
+
+import axios from "axios"
+
+//创建实例
+
+const service = axios.create({
+    baseURL:"",
+    timeout:5000
+})
+
+//请求拦截
+service.interceptors.request.use( config => {
+    return config
+},error=>{
+    return Promise.reject(error)
+})
+
+//响应拦截
+
+service.interceptors.response.use( response => {
+    return response
+},error => {
+    return Promise.reject(error)
+})
+
+export default service
